cfcca1bd935fde0c69d4dd2ed0cd2ac600ac45b7
MozammelHosen <mdmozammelhosen15701@gmail.com>
2023-01-03 06:42:26 +0600

    function

---
+++ arrayFunction.js
@@ -0,0 +1,20 @@
+// function
+function add1(num1, num2) {
+  const total = num1 + num2;
+  return total;
+}
+const number = add1(23, 26);
+console.log(number);
+
+// Function expression
+function add2(num1, num2) {
+  return num1 + num2;
+}
+const number2 = add2(23, 46);
+console.log(number2);
+
+// Function-asosycones
+
+function add3(num1,num2){
+    return num1 + num2;
+}
